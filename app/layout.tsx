46810580e83dab7f6e7c0e1eec4af4ed71c4dac4
raimonvibe <robertjanstefan@gmail.com>
2025-03-24 15:24:11 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,6 @@ export default function RootLayout({
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        {/* Eventueel extra stylesheets of scripts */}
       </head>
       <body>{children}</body>
     </html>
